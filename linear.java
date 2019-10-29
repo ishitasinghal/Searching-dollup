61bd161378da78706c793e549a41f9c2e75d1520
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-29 22:57:40 +0530

    Final tested code updated!

--- linear.java
+++ linear.java
@@ -1,5 +1,5 @@
 // searching linearly
-class Linear
+public class Linear
 {
 public static int search(int arr[], int item)
 {
@@ -13,11 +13,11 @@ return -1;
 public static void main(String args[])
 {
   int arr[]={1,54, 6, 34, 58, 91};
-  int item = 5;
-  int found ==search(arr, item);
+  int item = 4;
+  int found =search(arr, item);
   if(found==-1)
-    System.out.println("Element found");
-  else
     System.out.println("Element not found");
+  else
+    System.out.println("Element found");
 }
 }
