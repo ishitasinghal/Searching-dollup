b00af29feb8c23403d51f48e9a229b10827944f7
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-30 11:56:33 +0530

    Initial commit !

---
+++ binary.java
@@ -0,0 +1,18 @@
+public class Binary
+{
+    int bsearch(int arr[], int item)
+    {
+        int lower=0;
+        int upper = arr.length-1
+        while(lower<upper)
+        {
+            int mid=(lower+upper)/2;
+        }
+        if(arr[m]==item)       //item found
+        return m;
+        if(arr[m]>item)
+        {
+            arr.length-1
+        }
+    }
+}
