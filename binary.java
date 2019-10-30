b6acd4e512da07d9de8d73b017b9fa5f47709869
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-30 13:20:05 +0530

    Final tested code updated !

--- binary.java
+++ binary.java
@@ -4,7 +4,7 @@ class Binary
     int bsearch(int arr[], int item)
     {
         int lower=0;
-        int upper = arr.length-1
+        int upper = arr.length-1;
         while(lower<upper)
         {
         int mid=(lower+upper)/2;
@@ -12,7 +12,7 @@ class Binary
         return mid;
         if(arr[mid]>item)
         {
-            upper=mid-1;   //changing upperbound
+            upper=mid-1;   //changing upper bound
         }
         else
         {
@@ -25,9 +25,9 @@ class Binary
     {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	System.out.println("Enter the element to search");
+    	int item=Integer.parseInt(br.readLine());
         Binary b = new Binary();
         int arr[]={10, 23, 35, 46, 51, 62, 73};
-        int item=36;
         int found = b.bsearch(arr, item);
         if(found==-1)
         System.out.println("Item not found");
