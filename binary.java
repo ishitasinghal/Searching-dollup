fc27088a487d970f4510b6e0c1aa9b3abc22e8cb
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-30 13:11:36 +0530

    Commit 2 !

--- binary.java
+++ binary.java
@@ -6,13 +6,29 @@ public class Binary
         int upper = arr.length-1
         while(lower<upper)
         {
-            int mid=(lower+upper)/2;
-        }
-        if(arr[m]==item)       //item found
-        return m;
-        if(arr[m]>item)
+        int mid=(lower+upper)/2;
+        if(arr[mid]==item)       //item found
+        return mid;
+        if(arr[mid]>item)
         {
-            arr.length-1
+            upper=mid-1;   //changing upperbound
         }
+        else
+        {
+            lower=mid+1;   //changing lower bound
+        }
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        Binary b = new Binary();
+        int arr[]={10, 23, 35, 46, 51, 62, 73};
+        int item=36;
+        int found = b.bsearch(arr, item);
+        if(found==-1)
+        System.out.println("Item not found");
+        else
+        System.out.println("Item found at index "+ found);
     }
 }
