4291d412889d29072f96095ab812c76908523709
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-02 22:35:50 +0530

    Updating the basic concepts !

---
+++ jump.java
@@ -0,0 +1,3 @@
+#using jump algorithm to find the element.
+#we find the square root of the total size and that would be the number of jumps.
+#we jump to that number of indexes till a number greater than the linear search is acheived and then we start searching linearly backwards. 
