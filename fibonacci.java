a3c79e3238b987ee11fc14550b93ddc3161ac24f
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-03 23:15:57 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,4 +1,7 @@
 #This is searching via a fibonacci series.
 #this too works in a sorted array.
 #It uses fibonacci numbers to search for an element.
-
+#Used when array size is bigger and cannot fit in CPU cache.
+  #Divides array in unequal parts.
+  
+  
