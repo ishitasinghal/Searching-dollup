64ae5057338282ffd1b5b69239bac14e665d8c2b
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-05 11:44:01 +0530

    Updating fibonacci series !

--- fibonacci.java
+++ fibonacci.java
@@ -4,4 +4,22 @@
 #Used when array size is bigger and cannot fit in CPU cache.
   #Divides array in unequal parts.
   
+  #CODE
   
+  import java.util.*;
+class Fibos
+{
+  public static int min(int a, int b)
+    return(a<=b)? a:b);
+  public stattic int fibosearch(int arr[], int a, int n)
+  {
+    int f1 =0; //initializing first element of the fibonacci series
+    int f2 =1; //initializing second element of the fibonacci series
+    int f = f1+f2; //next fibo number
+    while(f<n)
+    {
+      f2=f1;
+      f1=f;
+      f=f2+f1;
+    }
+    
